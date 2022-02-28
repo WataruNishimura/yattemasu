98db6c3bcfe95abce275b4b628b2f2ecd69c18b6
WataruNishimura <wataru.nishimura@nnkk.work>
2022-02-28 22:52:14 +0900

    add declarative model

--- database.py
+++ database.py
@@ -1,17 +1,48 @@
 import os
+from tokenize import String
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import sqlalchemy
+from sqlalchemy import Column, Integer
 
 from modules.api import getValidChannelAccessToken, issueChannelAccessToken, verifyChannelAccessToken
 
+Base = sqlalchemy.declarative_base()
+
+
+class bot_account(Base):
+    __tablename__ = "bot_account"
+    client_id = Column(Integer, primary_key=True )
+    client_secret = Column(String)
+
+class channel_access_token_table(Base):
+    __tablename__ = "channel_access_token"
+    key_id = Column(Integer, primary_key=True)
+    token = Column(String)
+    client_id = Column(String)
+
+class bot_user_relation(Base):
+    __tablename__ = "bot_user_relation"
+class user_account(Base):
+    __tablename__ = "user_account"
+
+
 class Singleton(object):
+
     def __new__(cls, *args, **kargs):
         if not hasattr(cls, "_instance"):
             cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
-class DataBase(Singleton):
+
+class MysqlDatabase(Singleton):
+
+    def __init__(self, databaseURL, user, password):
+        self.engine = sqlalchemy.create_engine(databaseURL, echo=True)
+
+
+class FirebaseDataBase(Singleton):
 
     def __init__(self, credentialsFile, databaseURL):
         """In constructor, initialize firebase realtime database.
